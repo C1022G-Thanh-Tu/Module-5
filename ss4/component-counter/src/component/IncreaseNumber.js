81c05cb21d1943fdc32e87da5508be7e5626bf52
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-04-03 13:42:19 +0700

    update

--- ss4/component-counter/src/component/IncreaseNumber.js
+++ ss4/component-counter/src/component/IncreaseNumber.js
@@ -1,31 +1,27 @@
 import { useState } from "react";
 
-function IncreaseNumber() { 
-    const [count1, setCount1] = useState(1);
-    const [count2, setCount2] = useState(1);
+function IncreaseNumber() {
+  const [count1, setCount1] = useState(1);
+  const [count2, setCount2] = useState(1);
 
-    const handleIncreaseOneUnit = () => {
-        setCount1(count1+1);
-    }
+  const handleIncreaseOneUnit = () => {
+    setCount1(count1 + 1);
+  };
 
-    const handleIncreaseTwoUnit = () => {
-        setCount2(prev => prev +1);
-        setCount2(prev => prev +1);
-    }
+  const handleIncreaseTwoUnit = () => {
+    setCount2((prev) => prev + 1);
+    setCount2((prev) => prev + 1);
+  };
 
-    return (
-        <>
-            <h1>{count1}</h1>
-            <button onClick={handleIncreaseOneUnit}>
-                Add 1
-            </button>
+  return (
+    <>
+      <h1>{count1}</h1>
+      <button onClick={handleIncreaseOneUnit}>Add 1</button>
 
-            <h1>{count2}</h1>
-            <button onClick={handleIncreaseTwoUnit}>
-                Add 2
-            </button>
-        </>
-    )
- }
+      <h1>{count2}</h1>
+      <button onClick={handleIncreaseTwoUnit}>Add 2</button>
+    </>
+  );
+}
 
- export default IncreaseNumber;
+export default IncreaseNumber;
