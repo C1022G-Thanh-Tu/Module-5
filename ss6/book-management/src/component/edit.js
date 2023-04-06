7b1474701c2dcf7afd40880293d49b537a784af2
Thanhtu2607 <117065047+C1022G-Thanh-Tu@users.noreply.github.com>
2023-04-06 08:52:13 +0700

    update

--- ss6/book-management/src/component/edit.js
+++ ss6/book-management/src/component/edit.js
@@ -19,7 +19,20 @@ function Edit() {
   }, [param.id]);
 
   if (!book) {
-     return null;
+    return (
+      <Oval
+        height={80}
+        width={80}
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+        ariaLabel="oval-loading"
+        secondaryColor="#4fa94d"
+        strokeWidth={2}
+        strokeWidthSecondary={2}
+      />
+    );
   }
 
   return (
